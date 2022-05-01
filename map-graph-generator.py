f2bdb56f296d1323c20a56f35cb73d1b911f30d4
hzhhang <hzhang3839@gmail.com>
2022-04-30 20:48:21 -0700

    created file for map graph generator

---
+++ map-graph-generator.py
@@ -0,0 +1,7 @@
+# This script takes a black and white image as input, maps it onto an NxN grid,
+# and assigns infinite costs to all impossible paths (white squares)
+
+
+
+def get_image (filePath) :
+    print ("hello")
