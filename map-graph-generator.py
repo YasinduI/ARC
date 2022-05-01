902c41a60e0ac8dc7c57c13526dece6262bf91d6
hzhhang <hzhang3839@gmail.com>
2022-04-30 21:12:58 -0700

    added get_image to show image

--- map-graph-generator.py
+++ map-graph-generator.py
@@ -1,7 +1,13 @@
 # This script takes a black and white image as input, maps it onto an NxN grid,
 # and assigns infinite costs to all impossible paths (white squares)
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 
 
 def get_image (filePath) :
-    print ("hello")
+    image = plt.imread(filePath)
+    plt.show(image)
+
+get_image('C:\\Users\\User\\Desktop\\ARC\\test-image.png')
+
