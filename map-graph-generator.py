1dae6bc032a226b7795462c1307ecbd85dd26f71
hzhhang <hzhang3839@gmail.com>
2022-05-05 21:31:06 -0700

    added A*

--- map-graph-generator.py
+++ map-graph-generator.py
@@ -6,54 +6,9 @@ import matplotlib.ticker as plticker
 from PIL import Image
 import csv
 
-def grid_image (filePath, dpi, tick_interval) :
-    
-    """overlays gridlines onto a given image. likely not needed"""
-
-    # open image
-    image = Image.open(filePath)
-
-    # set dpi; smaller = thinner gridlines. 50 is good-ish
-    set_dpi = dpi
-
-    # image size
-    width = image.size[0]/set_dpi
-    height = image.size[1]/set_dpi
-
-    fig = plt.figure(figsize = (width, height), dpi = set_dpi)
-    axis = fig.add_subplot(1,1,1)
-
-    # removes whitespace from image
-    fig.subplots_adjust(left  = 0, right = 1, bottom = 0, top = 1)
-
-    # space between ticks
-    grid_interval = tick_interval
-    location = plticker.MultipleLocator(base = grid_interval)
-
-    # set major ticks
-    axis.xaxis.set_major_locator(location)
-    axis.yaxis.set_major_locator(location)
-
-    # add grid
-    axis.grid(which = 'major', axis = 'both', linestyle = '-')
-
-    # add the image
-    axis.imshow(image)
-    plt.imshow(image)
-
-    # axes and ticks
-    #plt.show()
-
-    # save the image
-    img_gridlined_path = 'C:\\Users\\User\\Desktop\\ARC\\ARC\\test-image-with-grid.png'
-    fig.savefig(img_gridlined_path, dpi = set_dpi)
-
-    # return file path of gridlined image
-    return img_gridlined_path
-
 def get_map (filePath) :
 
-    """second attempt at algo to get a .csv path cost map from image"""
+    """gets a .csv path cost map "cost_matrix" from a given image"""
 
     # open .csv file to write cost matrix, and create writer object
     costCSV = open('C:\\Users\\User\\Desktop\\ARC\\ARC\\nav-map-costs.csv', 'w', newline = '')
@@ -71,7 +26,7 @@ def get_map (filePath) :
     cost_matrix = [[999 for i in range(width)] for j in range(height)]
 
     # alter cost matrix to assign possible paths a cost of 1
-    # export csv
+    # export .csv map of paths costs
     for y in range (height) :
         for x in range (width) :
             if (pixel[x, y] != (255, 255, 255)) :
@@ -79,13 +34,138 @@ def get_map (filePath) :
 
         writer.writerow(cost_matrix[y])  
 
+    # close .csv map file
     costCSV.close()
 
 # main()
+class Node():
+    """A node class for A* Pathfinding"""
 
- #grid_image('C:\\Users\\User\\Desktop\\ARC\\ARC\\test-image.png', 50, 5)
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
 
-get_map('C:\\Users\\User\\Desktop\\ARC\\ARC\\test-image.png')
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+
+def main():
+
+    # create maze using external script
+    # overlay a NxN graph
+    # if square.hasWhite() : cost = 999999. These are impossible paths
+    # assign costs to nodes only because paths can have a cost of 1. since impossible paths have a cost of 999
+    
+    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 6, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    # depends on classroom locations
+    start = (0, 0)
+    end = (7, 6)
+
+    path = astar(maze, start, end)
+    print(path)
+    
+
+
+if __name__ == '__main__':
+    main()
+  #  get_map('C:\\Users\\User\\Desktop\\ARC\\ARC\\test-image.png')
 
 
 
